32dc12b986de2a9986e20585f3b6130e093a1074
fhnstephen <fhnstephen@gmail.com>
2024-11-21 16:34:34 -0500

    fix: deploy issues

--- api/src/libs/sequelize.ts
+++ api/src/libs/sequelize.ts
@@ -1,13 +1,15 @@
 import path from 'path';
 import { Sequelize } from 'sequelize-typescript';
 
+import { Profile } from '../models/profile';
+
 // Path to SQLite file
-const defaultDbPath = path.resolve(__dirname, '../../data/db.sqlite');
+const defaultDbPath = path.resolve(__dirname, './data/db.sqlite');
 export const dbPath = process.env.DB_PATH || defaultDbPath;
 
 // Initialize SQLite database
 export const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: dbPath, // Path to SQLite file
-  models: [path.resolve(__dirname, '../models')], // Register models
+  models: [Profile], // Register models
 });
